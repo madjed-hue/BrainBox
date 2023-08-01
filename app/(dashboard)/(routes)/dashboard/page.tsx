c99fb05f7fa6cecec5383e250c4c7bfeca91ac85
fateh beddiaf <kentounmax@gmail.com>
2023-08-01 01:57:11 +0100

    add sidebar component

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,3 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
-
 export default function DashboardPage() {
-  return (
-    <div>
-      dashboard (protected)
-      <UserButton afterSignOutUrl="/" />
-    </div>
-  );
+  return <div>dashboard (protected)</div>;
 }
