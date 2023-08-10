e7845612625d3a2bb47e2fc85a3a6f7e30d3d969
fateh beddiaf <kentounmax@gmail.com>
2023-08-10 14:39:10 +0100

    add landing content

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,13 +1,13 @@
 import { LandingNavbar } from "@/components/landing-navbar";
 import { LandingHero } from "@/components/landing-hero";
-// import { LandingContent } from "@/components/landing-content";
+import { LandingContent } from "@/components/landing-content";
 
 const LandingPage = () => {
   return (
     <div className="h-full ">
       <LandingNavbar />
       <LandingHero />
-      {/* <LandingContent /> */}
+      <LandingContent />
     </div>
   );
 };
